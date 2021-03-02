374d1f71849fe47e3887c21617d3ed99534b9c89
Cat-Denton <willwdenton@gmail.com>
2021-03-02 11:41:59 -0800

    add jquery link to roboRoger.js to fix a bug.

---
+++ src/js/roboRoger.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+
+export default function roboRoger(number) {
+
+  if (number > 100) {
+    document.getElementById("roboRogerTalk").style.color = "red";
+    $("#roboRogersPic").show();
+    return "Mr. roboRogers doesn't have all day! Input a smaller number so they don't get angry!"
+  }
+
+  $("#roboRogersPic").hide();
+  const numArray = [];
+
+  for (let index = 0; index <= number; index++) {
+    if(index.toString().includes(3)) {
+      numArray[index] = "Won't you be my neighbor?";
+    } else if (index.toString().includes(2)) {
+      numArray[index] = "Boop!";
+    } else if (index.toString().includes(1)) {
+      numArray[index] = "Beep!";
+    } else {
+      numArray[index] = index.toString();
+    }
+  }
+
+  let numString = "";
+
+  if (number === 69) {
+    numArray.forEach(function(element) {
+      numString = numString + element + " ";
+    });
+    numString += "...nice ";
+  } else {
+    numArray.forEach(function(element) {
+      numString = numString + element + " ";
+    });
+  }
+  document.getElementById("roboRogerTalk").style.color = "black";  
+  //this removes an extraneous space from the end of numString and returns it
+  return numString.slice(0,numString.length - 1);
+  
+}
