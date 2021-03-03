44064c7ea833c9b2413062703d0d9c4bfcbe2c47
Cat-Denton <willwdenton@gmail.com>
2021-03-02 19:28:54 -0800

    first test passed, all UI logic separated out of roboRoger.js into main.js
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,32 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import roboRoger from './js/roboRoger.js';
 
+$(document).ready(function() {
+  $("#numberForm").submit(function(event) {
+    
+    const num = $("input#number").val();
 
+    const story = roboRoger(parseInt(num));
+    
+
+    if (story === "Mr. roboRogers doesn't have all day! Input a smaller number so they don't get angry!") {
+      document.getElementById("roboRogerTalk").style.color = "red";
+      $("#roboRogersPic").show();
+    }
+
+    document.getElementById("roboRogerTalk").style.color = "black";
+
+    $("#roboRogersPic").hide();
+
+    $("#roboRogerTalk").text(story);
+
+    $("#firstButton").hide();
+
+    $("#repeatButton").show();
+    
+    event.preventDefault();
+  });
+});
