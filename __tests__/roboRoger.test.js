0bc3a9efa7d08e3e987dcc50b550a1fb8edb1c35
Cat-Denton <willwdenton@gmail.com>
2021-03-02 19:37:41 -0800

    second test passed. Removed jquery import from roboRoger.test.js.
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- __tests__/roboRoger.test.js
+++ __tests__/roboRoger.test.js
@@ -1,11 +1,12 @@
 import { TestScheduler } from 'jest';
 import roboRoger from './../src/js/roboRoger.js';
-import $ from 'jquery';
-
-
 
 describe('roboRoger', () => {
   test('Should output an error if the input is >100', ()=> {
     expect(roboRoger(102)).toEqual("Mr. roboRogers doesn't have all day! Input a smaller number so they don't get angry!");
+  });
+
+  test("Should output \"0\" if the input is 0", ()=> {
+    expect(roboRoger(0)).toEqual("0");
   });
 });
