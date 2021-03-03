a83e80d2d30a499c67eab98e3bf05a6b2a4b92e7
Cat-Denton <willwdenton@gmail.com>
2021-03-02 20:02:41 -0800

    Passed test for number replacement priority.
    Co-authored-by: Saoud Rana <saoud@saoud.dev>

--- __tests__/roboRoger.test.js
+++ __tests__/roboRoger.test.js
@@ -13,4 +13,15 @@ describe('roboRoger', () => {
   test("Should replace all numbers with a numeral 1 with \"beep\"", ()=> {
     expect(roboRoger(1)).toEqual("0 Beep!");
   });
+
+  test("Should replace all numbers with a numberal 2 with \"boop\"", ()=> {
+    expect(roboRoger(2)).toEqual("0 Beep! Boop!");
+  });
+
+  test("Should replace all numbers with a numeral 3 with \"Won't you be my neighbor?\"", ()=> {
+    expect (roboRoger(3)).toEqual("0 Beep! Boop! Won't you be my neighbor?")
+  });
+  test("Should prioritize replacing numbers with 3 over 2 over 1", ()=> {
+    expect(roboRoger(13)).toEqual("0 Beep! Boop! Won't you be my neighbor? 4 5 6 7 8 9 Beep! Beep! Boop! Won't you be my neighbor?")
+  })
 });
